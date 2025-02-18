565b1a9aabf440d0eff4f5a5684be1ae20dad128
최수호 <csw020106@naver.com>
2025-02-19 08:27:23 +0900

    25.02.19 백준 단계별 1단계 : 입출력과 사칙연산

---
+++ Baekjun/stage 1/2557.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* 문제 : Hello World!를 출력하시오. */
+
+/* 입력 : X */
+
+/* 출력 : Hello World!를 출력하시오. */
+
+/* 예제 입력 1 : X             예제 출력 1  : Hello World! */
+
+int main(void){
+    
+    printf("Hello World!");    
+}
